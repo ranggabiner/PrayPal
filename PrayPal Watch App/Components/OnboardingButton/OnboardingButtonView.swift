16859ea05e42718a0316145ef9b40fd6db8f83a9
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-05-24 16:33:40 +0700

    latest

--- PrayPal Watch App/Components/OnboardingButton/OnboardingButtonView.swift
+++ PrayPal Watch App/Components/OnboardingButton/OnboardingButtonView.swift
@@ -101,8 +101,7 @@ struct OnboardingButtonView: View {
         }
 
         // Schedule notifications three times with 5 minutes interval
-//        let intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25] // in minutes
-        let intervals = Array(0...100) // in minutes
+        let intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25] // in minutes
         for interval in intervals {
             guard let notificationDate = calendar.date(byAdding: .minute, value: interval, to: scheduledTime) else {
                 print("Failed to calculate notification time for interval \(interval) minutes.")
